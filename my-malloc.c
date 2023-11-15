5f77e61094574cb05d46ec85ddc5db5003262f86
Solano <hsolano@weathertop.cs.middlebury.edu>
2023-11-15 02:55:35 -0500

    more progress on theory

--- my-malloc.c
+++ my-malloc.c
@@ -18,11 +18,11 @@ typedef struct Footer {
 //12 bytes (ints are 4, pointers are 4)
 typedef struct Flist {
 int size; //size of empty (free) space
-	struct flist *next; //pointer to next free region
-	struct flist *prev; //pointer to prev free region
+	struct Flist *next; //pointer to next free region
+	struct Flist *prev; //pointer to prev free region
 } *pFLIST; //pointer to struct flist
 
-static HEAD = NULL; //global //don't wanna use heap memory to intialize this + every function2 needs to know it?
+static void *HEAD = NULL; //global //don't wanna use heap memory to intialize this + every function2 needs to know it?
 
 
 // The malloc() function allocates size bytes and returns a pointer
@@ -31,26 +31,43 @@ static HEAD = NULL; //global //don't wanna use heap memory to intialize this + e
 //        be successfully passed to free().
 
 
+
 void *malloc(size_t size) {
+
+	/* If empty, initialize free list */
 	if (HEAD == NULL) { //if free list is EMPTY
-		//move program break _x_ bytes: Initial memory allocation w/ syscall
-		sbrk(250);
+		//move program break _x_ bytes: Initial memory allocation w/ syscall; sbrk() returns pointer to start of newly allocated memory
+		HEAD = sbrk(250);
+		//Initialize free list (one big node) 
+		pFLIST initial;
+		initial = (pFLIST) HEAD; //why the cast? //struct MyStruct instance; //struct MyStruct* ptr = &instance;
+		initial->prev = NULL;
+		initial->next = NULL; //current location of program break//won't call this ever again? do we rlly need it tho? i guess we need the bounds of our available memory //can't we use size to determine this, maybe not cuz it's unordered?
+		initial->size = HEAD - sbrk(0); //current location of program break - start of newly allocated memory
+		//size? next-prev (only for initial node actually makes sense)
 	}
 
-	/* traverse free list for big enough chunk */
+	/* traverse free list for big enough chunk */ //use pointer arithmetic to traverse? move from one struct to next? //
 
-	/* if we find big enough chunk, use it */
+	while (//there's chunks left node-> next != NULL; node-> next != sbrk(0), seems wasteful)
+	
+		/* if we find big enough chunk, use it */
+		if(initial->size > size + struct overhead) {
+			
+		}
 
-		/* if it's too big (measure for this if size * 2 < free space, use half for malloc and stash other half?), split it, then use it */ -> func for this? 
+			/* if it's too big (measure for this if size * 2 < free space, use half for malloc and stash other half?), split it, then use it */ -> func for this? 
 	
 	/* if we don't, call sbrk or brk to move program break and ultimately get new free chunk, use it */
 
-		/* if it's too big, split it, then use it */ -> func for this?
+			/* if it's too big, split it, then use it */ -> func for this?
 
 
 	//int tail = sbrk(135168); //don't need to keep track of end? i feel like we should
 	//initialize first node to give to user
 	//other node pointing to end
+
+	return HEAD;
 }
 
 
@@ -82,7 +99,9 @@ void free(void *ptr) {
 
 
 int main(int argc, char *argv[]) {
-	printf("work");
+	//printf("work");
+	printf(HEAD);
+	//printf("fake news");
 }
 
 
