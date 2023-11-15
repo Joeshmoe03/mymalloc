79c7611d629f38cbf86ae7487e5eff95a07a892d
Solano <hsolano@weathertop.cs.middlebury.edu>
2023-11-14 23:48:04 -0500

    progress on theory

--- my-malloc.c
+++ my-malloc.c
@@ -1,38 +1,28 @@
 /* my-malloc.c */
 #include <stdio.h>
 #include <unistd.h>
-
-#include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
 
-int main()
-{
-  int *i1, *i2;
-
-  printf("sbrk(0) before malloc(4): 0x%x\n", sbrk(0));
-
-  i1 = (int *) malloc(4);
-  printf("sbrk(0) after `i1 = (int *) malloc(4)': 0x%x\n", sbrk(0));
-
-  i2 = (int *) malloc(4);
-  printf("sbrk(0) after `i2 = (int *) malloc(4)': 0x%x\n", sbrk(0));
-
-  printf("i1 = 0x%x, i2 = 0x%x\n", (unsigned int) i1, (unsigned int) i2);
-}
-
 /* Linked List Metadata */
 
-//16 bytes (ints are 4, pointers are 4)
-typedef struct node {
-    int size; //size of current allocation
-    int isfree;
-    struct node *next; //pointer to next allocation
-    struct node *prev; //pointer to previous allocation
 
-} *NODE; //pointer to struct node 
+typedef struct Header {
+	int size; //size (of allocated chunk)
+} *pHEADER; //pointer to struct node 
 
-NODE head = NULL; //global - pointer to the head node //don't wanna use heap memory to intialize this + every function2 needs to know it?
+typedef struct Footer {
+	int size; //size of previous chunk (only used if prev is free)
+} *pFOOTER; //pointer to struct node 
+
+//12 bytes (ints are 4, pointers are 4)
+typedef struct Flist {
+int size; //size of empty (free) space
+	struct flist *next; //pointer to next free region
+	struct flist *prev; //pointer to prev free region
+} *pFLIST; //pointer to struct flist
+
+static HEAD = NULL; //global //don't wanna use heap memory to intialize this + every function2 needs to know it?
 
 
 // The malloc() function allocates size bytes and returns a pointer
@@ -42,17 +32,57 @@ NODE head = NULL; //global - pointer to the head node //don't wanna use heap mem
 
 
 void *malloc(size_t size) {
-    if (head == NULL) {
-        //move program break x bytes: Initial memory allocation w/ syscall
-        //set HEAD to smth
-        int tail = sbrk(135168); //don't need to keep track of end? i feel like we should
-        //initialize first node to give to user
-        //other node pointing to end
-    }
+	if (HEAD == NULL) { //if free list is EMPTY
+		//move program break _x_ bytes: Initial memory allocation w/ syscall
+		sbrk(250);
+	}
+
+	/* traverse free list for big enough chunk */
+
+	/* if we find big enough chunk, use it */
+
+		/* if it's too big (measure for this if size * 2 < free space, use half for malloc and stash other half?), split it, then use it */ -> func for this? 
+	
+	/* if we don't, call sbrk or brk to move program break and ultimately get new free chunk, use it */
+
+		/* if it's too big, split it, then use it */ -> func for this?
+
+
+	//int tail = sbrk(135168); //don't need to keep track of end? i feel like we should
+	//initialize first node to give to user
+	//other node pointing to end
 }
 
+
+
+
+    // The free() function frees the memory space pointed to by ptr,
+    // which must have been returned by a previous call to malloc() or
+    // related functions.  Otherwise, or if ptr has already been freed,
+    // undefined behavior occurs.  If ptr is NULL, no operation is
+    // performed.
+
+void free(void *ptr) {
+	/* Insert at front w/ LIFO */
+
+	/* 4 cases: */
+
+	/* Case 1: NEITHER next/prev is free, just put freed (current) chunk in front */
+
+	/* Case 2: prev is free, combine prev w/ freed (current) doing pointer updating magic dance, then put it in front */
+
+	/* Case 3: next is free, combine next w/ freed (current) doing pointer updating magic dance, then put it in front */
+
+	/* Case 4: next && prev are free, combine BOTH w/ freed (current) doing pointer updating magic dance, then put it in front */
+
+
+}
+
+
+
+
 int main(int argc, char *argv[]) {
-    printf("work");
+	printf("work");
 }
 
 
